659258cbffb3f18745e4ed18ed4b3c48210f8562
Ivandx12 <in9150915@gmail.com>
2025-03-01 15:13:18 -0600

    Agregando cambios en el código

--- ejercicio10.c
+++ ejercicio10.c
@@ -3,10 +3,36 @@
 
 void ordenarBurbuja(int *arr, int tamaño) {
     // Implementar algoritmo de ordenamiento aquí
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+    }
+}
+void imprimirArreglo(int *arr, int tamaño) {
+    for (int i = 0; i < tamaño; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
 }
 
 int main() {
     int arr[] = {34, 12, 56, 78, 23};
     // Código para ordenar y mostrar el arreglo
+    int tamaño = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Arreglo original: ");
+    imprimirArreglo(arr, tamaño);
+
+    ordenarBurbuja(arr, tamaño);
+
+    printf("Arreglo ordenado: ");
+    imprimirArreglo(arr, tamaño);
+
     return 0;
+
 }
