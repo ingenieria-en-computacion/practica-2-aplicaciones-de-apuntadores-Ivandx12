1224ed8370057fdfd38045d00d5cc3aa62eed445
Ivandx12 <in9150915@gmail.com>
2025-03-01 15:06:09 -0600

    Agregando cambios en el código

--- ejercicio9.c
+++ ejercicio9.c
@@ -1,13 +1,20 @@
 // 9. Suma con Apuntadores
 #include <stdio.h>
 
-int sumaArreglo(int *arr, int tamaño) {
+int sumaArreglo(int *arr, int tam){
     // Implementar la suma aquí
-    return 0;
+    int suma = 0;
+    for (int i = 0; i < tam; i++) {
+        suma += *(arr + i);  
+    }
+    return suma;
 }
 
-int main() {
-    int numeros[] = {1, 2, 3, 4, 5};
-    // Código para calcular y mostrar la suma
+int main(){
+    int numeros[j] = {1, 2, 3, 4, 5};
+    // Código para calcular y mostrar la suma 
+    int tam = sizeof(numeros) / sizeof(numeros[0]);
+    int resultado = sumaArreglo(numeros, tam);
+    printf("La suma del arreglo es: %d\n", resultado);
     return 0;
 }
