8a3ad4880b469f0a9a1f29455173d8771a6400ce
Ivandx12 <in9150915@gmail.com>
2025-03-01 14:54:26 -0600

    Agregando cambios en el código

--- ejercicio7.c
+++ ejercicio7.c
@@ -4,5 +4,9 @@
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     // Código para imprimir direcciones de memoria aquí
+    printf("holo\n");
+    for(int i = 0; i < 5; i++){
+        printf("arr[%d] está en la dirección: %p\n", i, &arr[i]);
+    }
     return 0;
 }
--- ejercicio8.c
+++ ejercicio8.c
@@ -1,12 +1,21 @@
-// Intercambio de Valores
 #include <stdio.h>
 
+// Función para intercambiar valores usando punteros
 void intercambiar(int *a, int *b) {
-    // Implementar intercambio aquí
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main() {
     int arr[] = {5, 10};
-    // Código para llamar a la función y mostrar los valores intercambiados
+
+    printf("Antes del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+
+    // Llamamos a la función pasando las direcciones de los elementos
+    intercambiar(&arr[0], &arr[1]);
+
+    printf("Después del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+
     return 0;
 }
